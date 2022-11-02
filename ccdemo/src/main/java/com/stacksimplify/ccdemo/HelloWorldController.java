08e269ef5aa34757c7b563741ca9ed481c0954de
thadeu <marciothadeu1984@gmail.com>
2022-11-02 18:55:34 -0300

    second commit

--- ccdemo/src/main/java/com/stacksimplify/ccdemo/HelloWorldController.java
+++ ccdemo/src/main/java/com/stacksimplify/ccdemo/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Manual Approval Stage and Prod Deployment - v2";
+		return "Manual Approval Stage and Prod Deployment - v5";
 	}
 	
 }
